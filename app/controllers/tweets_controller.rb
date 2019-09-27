768506f81b6e7faedb956f4f1e19cf262bc75099
Felipe Austríaco <felipe1180@hotmail.com>
2019-09-27 16:27:01 -0300

    new column Popularity

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,6 +7,7 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all.order("created_at DESC")
     @tweet = Tweet.new
+    @user = User.new
   end
 
   # GET /tweets/1
