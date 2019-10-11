12360a447c900ec2874e3286196cde57085e8eb5
Felipe Austríaco <felipe1180@hotmail.com>
2019-10-11 15:58:17 -0300

    new contributor added - chileno

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,9 +1,5 @@
 class ProfileController < ApplicationController
-	def index
-<<<<<<< HEAD
-	end
-=======
-		@tweet = Tweet.new
-	end	
->>>>>>> 6fbdda85d2a0d6bcb3707a6009b1ecd31824804d
+  def index
+    @tweet = Tweet.new
+  end
 end
